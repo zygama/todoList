1a7c783418fbffcfe313c7d7fa77f1f97581f9fa
sofiane <noxxys.lam@gmail.com>
2018-03-08 06:34:33 +0100

    HTML is working, let's now do some styling

--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -3,36 +3,42 @@ var inputTodos = $('input');
 var todosList = $('.todo');
 var deleteItemButtons = $('.delete_item_button');
 
+
+// Will append the new todo to the list with a fade effect
 function appendTodo(p_todo) {
-  $('ul').append(
-    `<li class="todo">
-      <span class="delete_item_button">X</span>
-      ${p_todo}
-    </li>`
-  );
+  let itemToAdd = $(`<li class="todo">
+                      <span class="delete_item_button">X</span>
+                        ${p_todo}
+                      </li>`
+                    ).hide().fadeIn(1000); // Trick is to hide it and fadeIn it in the jQuery Object
+
+  $('ul').append(itemToAdd); // We just have to append now
 }
 
 function initListeners() {
   // Toggle class 'did_todo' on a todo when clicked
-  todosList.on('click', function() {
+  $('ul').on('click', 'li', function() {
+    // Note: To fire on new todos added I have to listen on ul (existing when initializing listener)
+    // and check if this is a li clicked
     $(this).toggleClass('did_todo');
   });
 
   // Delete todo when the delete button of this todo is clicked
-  deleteItemButtons.on('click', function() {
-    // let indexTodo = $(this).index('.delete_item_button');
-    // todosList.eq(indexTodo).remove();
+  $('ul').on('click', 'span.delete_item_button', function() {
+    // Note: Have to listen to an existing parent first and check if element clicked is 'span.delete_item_button'
     $(this).parent().fadeOut(400, function() {
       $(this).remove();
     });
+    // let indexTodo = $(this).index('.delete_item_button');
+    // todosList.eq(indexTodo).remove();
   });
 
   // Add a new todo with the content of the text input when user hit 'Enter'
   inputTodos.on('keypress', function(event) {
-    if (event.which === 13 && inputTodos.val().length >= 2) {
+    if (event.which === 13 && $(this).val().length >= 2) {
       console.log("append");
-      appendTodo(inputTodos.val());
-      inputTodos.val('');
+      appendTodo($(this).val());
+      $(this).val(''); // Clear the input when todo is added
     }
   });
 }
